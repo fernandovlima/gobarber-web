4a9fba4ab5b0455b7f77ba2f4f67d6a33a2b52a9
fernandovlima <fernandovlima.s@gmail.com>
2019-11-11 20:42:31 -0300

    add layouts

---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-80deg, #3e9d8a, #6cf4d9);
+`;
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+// layuots
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const signed = true;
   // se nao logado e rota privada redireciona para o login
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
@@ -17,8 +21,18 @@ export default function RouteWrapper({
     return <Redirect to="/dashboard" />;
   }
 
+  const Layout = signed ? DefaultLayout : AuthLayout;
   // se tiver logado
-  return <Route {...rest} component={Component} />;
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
