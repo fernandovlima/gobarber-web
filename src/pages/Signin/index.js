f8fe5a7b521475fabb6dbc6038c28da706a970d8
fernandovlima <fernandovlima.s@gmail.com>
2019-11-17 14:39:37 -0300

    user reducer with authentication persistence

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
@@ -17,6 +17,7 @@ const schema = Yup.object().shape({
 
 export default function Signin() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -27,13 +28,13 @@ export default function Signin() {
       <img src={logo} alt="gobarber" />
 
       <Form schema={schema} onSubmit={handleSubmit}>
-        <Input name="email" type="email" placeholder="Seu E-mail" />
+        <Input name="email" type="email" placeholder="seu e-mail" />
         <Input
           name="password"
           type="password"
           placeholder="sua senha secreta"
         />
-        <button type="submit">LOGIN</button>
+        <button type="submit">{loading ? 'loading...' : 'LOGIN'}</button>
         <Link to="/register">create account</Link>
       </Form>
     </>
