55bb9d06405a28a011961854675d55d91d6c4496
fernandovlima <fernandovlima.s@gmail.com>
2019-11-17 14:47:11 -0300

    add toast for login

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
@@ -17,7 +18,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('User is not a provider');
+      toast.error('user is not a provider');
       return;
     }
 
@@ -25,6 +26,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (error) {
+    toast.error('authentication error, please check your login information.');
     yield put(signFailure());
   }
 }
